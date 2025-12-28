0879827426b1ddc3ca70d61850b94556b0de21dd
sohal <tosohal@gmail.com>
2025-12-28 18:32:21 +0100

    s

--- cmake/deps.stm32cubeh7.cmake
+++ cmake/deps.stm32cubeh7.cmake
@@ -2,4 +2,4 @@
 set(BUILD_stm32cubeh7_LIBRARY FALSE CACHE BOOL "Build STM32CubeH7 library" FORCE)
 
 # Include generic STM32Cube silicon support from cmake_scripts
-include(${cmake_scripts_SOURCE_DIR}/silicon/stm32cubexx.cmake)
+include(${cmake_scripts_SOURCE_DIR}/silicon/st/stm32cubexx.cmake)
--- cmake/deps.toolchain.cmake
+++ cmake/deps.toolchain.cmake
@@ -1,12 +1,8 @@
-# Fetch cmake_scripts (toolchain and silicon support) via CPM
+# dependency on the toolchain is brought by CPM as a package
 CPMAddPackage(
-    NAME cmake_scripts
-    GITHUB_REPOSITORY kodezine/cmake_scripts
-    GIT_TAG ${GITHUB_BRANCH_toolchain}
+  NAME cmake_scripts
+  URL https://github.com/kodezine/cmake_scripts/archive/refs/tags/${GITHUB_BRANCH_toolchain}.tar.gz
+  URL_HASH SHA256=${GITHUB_BRANCH_toolchain_SHA256}
+  OPTIONS
     DOWNLOAD_ONLY TRUE
-    GIT_SHALLOW TRUE
 )
-
-if(cmake_scripts_ADDED)
-    message(STATUS "cmake_scripts fetched to: ${cmake_scripts_SOURCE_DIR}")
-endif()
